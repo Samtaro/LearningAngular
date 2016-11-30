315266f9ef6795359228be622be0ccc9215ea441
Sam <samtarowong@gmail.com>
2016-11-30 00:19:24 -0500

    Added a form to add reviews using a review controller.

--- app.js
+++ app.js
@@ -24,6 +24,13 @@
         }
     };
   });
+  app.controller('ReviewController', function(){
+   this.review = {};
+   this.addReview=function(product){
+     product.reviews.push(this.review);
+     this.review={};
+   };
+ });
   var gems = [{
       canPurchase: true,
       name: 'Azurite',
