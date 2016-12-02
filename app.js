8679f6b4cad559af398827ce9c2b01fffbf8cf0a
Sam <samtarowong@gmail.com>
2016-12-02 02:35:38 -0500

    Added proper boostrap css. Added custom directive.

--- app.js
+++ app.js
@@ -33,6 +33,18 @@
      this.review={};
    };
  });
+ app.directive("productDescription", function(){
+    return {
+      restrict: "E",
+      templateUrl: "product-description.html"
+      };
+  });
+  app.directive("productSpecs", function(){
+    return {
+      restrict: "A",
+      templateUrl: "product-specs.html"
+    };
+  });
   var gems = [{
       canPurchase: true,
       name: 'Azurite',
